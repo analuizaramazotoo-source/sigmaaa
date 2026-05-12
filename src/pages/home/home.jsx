2166049da25f94774da053077869de7e28fcb98f
ana <analuizaramazotoo@gmail.com>
2026-05-12 11:37:57 -0300

    teste 12 05 a

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,29 @@
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="container">
+
+      <header className="topbar">
+        <h1>SECRETARIA DO MEIO AMBIENTE</h1>
+      </header>
+
+      <main className="main">
+
+        <h2>Crie sua conta</h2>
+
+        <p>
+          Registre-se e conecte-se ao portal
+        </p>
+
+        <button>
+          Criar conta
+        </button>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default Home;
