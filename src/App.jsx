1cef8574c29861c7cf5675f8e8dffd2d1987abc5
ENZO NATHAN OLIVEIRA MOSQUINI <enzo.mosquini>
2026-05-15 15:19:23 -0300

    15 05 ae

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/home/home";
+import CadastroCidadao from "./pages/auth/cadastrocidadao/cadastrocidadao";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/CadastroCidadao" element={<CadastroCidadao />} />
+
       </Routes>
     </Router>
   );
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./home.module.css";
 
 //import img from '../../assets/secre.png'
@@ -56,14 +57,14 @@ function Home() {
               <h3>Equipe</h3>
               <span>Membros das equipes</span>
               <button>Selecionar</button>
-             
+              
             </div>
 
             <div className="card">
               <img src="/img cidadão.png" className="card-img" alt="Cidadão" />
               <h3>Cidadão</h3>
               <span>Solicitação de serviços</span>
-              <button>Selecionar</button>
+              <Link to= "/CadastroCidadao"><button>Selecionar</button></Link>
 
             </div>
 
