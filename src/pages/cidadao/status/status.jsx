e5d55cd449befec5699964a7984fd8c14d2af951
Joaopedro17s <joaopedrosoaresgomes13@gmail.com>
2026-06-15 14:18:09 -0300

    15/06
    comit

--- src/pages/cidadao/status/status.jsx
+++ src/pages/cidadao/status/status.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import styles from "./Status.module.css";
 import bannerFundo from "../../../assets/banner.png";
 import arvoreLogo from "../../../assets/arvore.png";
