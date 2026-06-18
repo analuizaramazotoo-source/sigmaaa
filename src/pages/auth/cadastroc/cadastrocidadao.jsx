717cc261e3b74bc0eed69379bff771634e759c65
ana <analuizaramazotoo@gmail.com>
2026-06-18 09:26:20 -0300

    18 06 a

--- src/pages/auth/cadastroc/cadastrocidadao.jsx
+++ src/pages/auth/cadastroc/cadastrocidadao.jsx
@@ -2,25 +2,20 @@ import styles from "./cadastrocidadao.module.css";
 import { Link } from "react-router-dom";
 
 function CadastroCidadao() {
-
   return (
-
     <div className={styles["cadastro-page"]}>
-
       {/* TOPO */}
       <div className={styles.topo}>
-
-        <div className={styles["topo-esquerda"]}>
-
-          <img
-            src="/secre.png"
-            alt="logo"
-          />
-
-          <span>
-            SECRETARIA DO MEIO AMBIENTE
-          </span>
-
+        <div className={styles.logo}>
+          <div className={styles["header-logos"]}>
+            <div className={styles.logo}>
+              <img
+                src="secretaria meio ambiente.png"
+                className={styles.logos}
+                alt="Secretaria do Meio Ambiente"
+              />
+            </div>
+          </div>
         </div>
 
         {/* BOTÃO VOLTAR */}
@@ -30,42 +25,42 @@ function CadastroCidadao() {
         >
           Voltar
         </button>
-
       </div>
 
       {/* CONTEÚDO */}
       <div className={styles.conteudo}>
-
         {/* LATERAL */}
         <div className={styles.lateral}>
-
           <img
-            src="/prefeitura.png"
-            className={styles["logo-prefeitura"]}
+            src="logotupa.png"
+            className={styles["logo-img"]}
+            alt="Prefeitura de Tupã"
           />
 
-          <div className={styles["logo-inferior"]}>
+          {/* LINHAS DECORATIVAS SUPERIORES */}
+          <div className={styles["linha-grande"]}></div>
+          <div className={styles["linha-pequena"]}></div>
 
-            <img src="/meio-ambiente.png" />
+          {/* LOGO INFERIOR */}
+          <div className={styles["logo-inferior"]}>
+            <img src="/meio-ambiente.png" alt="Meio Ambiente" />
 
             <span>
               SECRETARIA DO
               <br />
               MEIO AMBIENTE
             </span>
-
           </div>
 
+          {/* LINHAS DECORATIVAS INFERIORES */}
+          <div className={styles["linha-grande"]}></div>
+          <div className={styles["linha-pequena"]}></div>
         </div>
 
         {/* CENTRO */}
         <div className={styles.centro}>
-
           <div className={styles.card}>
-
-            <h2>
-              Cadastro do Cidadão
-            </h2>
+            <h2>Cadastro do Cidadão</h2>
 
             <p className={styles.sub}>
               Crie sua conta para relatar problemas ou solicitar serviços
@@ -73,20 +68,13 @@ function CadastroCidadao() {
             </p>
 
             <div className={styles["form-area"]}>
-
-              {/* INPUTS */}
               <div className={styles.inputs}>
-
                 <input placeholder="Nome Completo" />
-
                 <input placeholder="E-mail" />
-
                 <input placeholder="Celular" />
-
                 <input placeholder="CPF" />
 
                 <div className={styles.senha}>
-
                   <input
                     placeholder="Senha"
                     type="password"
@@ -96,38 +84,27 @@ function CadastroCidadao() {
                     placeholder="Confirmar Senha"
                     type="password"
                   />
-
                 </div>
 
-              <Link to = "/cidadao">  <button className={styles["btn-cadastrar"]}>
-                  CADASTRAR
-                </button>
+                <Link to="/cidadao">
+                  <button className={styles["btn-cadastrar"]}>
+                    CADASTRAR
+                  </button>
                 </Link>
-{/* IMAGEM */}
-              
-
               </div>
-
-              
             </div>
+
             <div className={styles.imagem}>
-
-                <img
-                  src="/cadastrocidadao.png"
-                  alt="segurança"
-                />
-
-              </div>
+              <img
+                src="/cadastrocidadao.png"
+                alt="Segurança"
+              />
+            </div>
           </div>
-
         </div>
-
       </div>
-
     </div>
-
   );
-
 }
 
 export default CadastroCidadao;
