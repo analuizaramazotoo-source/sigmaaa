198836132a75a59a8d6cd2c48134e03c307c4c87
ana <analuizaramazotoo@gmail.com>
2026-08-18 10:33:39 -0300

    18 08 a

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const UsuariosController = require('../controllers/usuario');
 
 router.get('/usuario', UsuariosController.listarUsuarios);
+router.post('/usuario', UsuariosController.cadastrarUsuarios);
+router.patch('/usuario', UsuariosController.editarUsuarios);
+router.delete('/usuario', UsuariosController.apagarUsuarios);
 
 module.exports = router;
