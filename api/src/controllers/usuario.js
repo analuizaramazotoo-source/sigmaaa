50be7163a6b30809c6f558acd19d3de445fa504f
ana <analuizaramazotoo@gmail.com>
2026-09-03 08:36:08 -0300

    0309a

--- api/src/controllers/usuario.js
+++ api/src/controllers/usuario.js
@@ -3,11 +3,18 @@ const db = require('../database/connection');
 module.exports = {
   async listarUsuarios(request, response) {
     try {
+      // instruções SQL
+      const sql = `SELECT
+      usu_id, usu_nome, usu_email, usu_dt_nasc, usu_senha,
+      usu_tipo, usu_ativo
+      FROM usuarios;`;
+      // executa instruções SQL e armazena o resultado na variável usuário
+      const usuarios = await db.query(sql);
       // throw new Error('EU causei o erro');
       return response.status(200).json({
         sucesso: true,
         mensagem: 'Lista de usuários.',
-        dados: null
+        dados: usuarios
       });
     } catch (error) {
       return response.status(500).json({
